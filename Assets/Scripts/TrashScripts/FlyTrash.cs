777a0f624bb2ad6a21ab54824bf3a98effaba651
Promen <klimov7898@gmail.com>
2024-11-10 23:24:36 +0500

    UpdateFlyTrash + Больше спрайтов

--- Assets/Scripts/TrashScripts/FlyTrash.cs
+++ Assets/Scripts/TrashScripts/FlyTrash.cs
@@ -4,33 +4,103 @@ using UnityEngine;
 
 public class FlyTrash : Trash
 {
-    private float speed = 3f;
-    new private Rigidbody2D rigidbody;
-    Vector2 RandomMovement;
-    private Rigidbody2D rb;
+   /* ////    public float speed = 3f;
+    ////    new private Rigidbody2D rigidbody;
+    ////    public float checkRadius = 0.5f; // Радиус проверки столкновений
+    ////    public LayerMask obstacleLayer; // Слой, на котором находятся препятствия
+    //   private Vector2 direction;
 
-    // Start is called before the first frame update
+    //    // Start is called before the first frame update
+    //    void Start()
+    //    {
+    //        rigidbody = GetComponent<Rigidbody2D>();
+    //        SetRandomDirection();
+    //    }
+    //    private void OnCollisionEnter2D(Collision2D collision)
+    //    {
+    //        if (collision.gameObject.GetComponent<metla>())
+    //        {
+    //            rigidbody.gravityScale = 1;
+    //            speed = 0;
+    //        }
+    //    }
+
+    //    // Update is called once per frame
+    //    void Update()
+    //    {
+    //        if (Physics2D.OverlapCircle(transform.position, checkRadius, obstacleLayer))
+    //        {
+    //            // Если есть столкновение, выбираем новое случайное направление
+    //            SetRandomDirection();
+    //        }
+    //        transform.Translate(direction * speed * Time.deltaTime);
+    //    }
+    //    private void SetRandomDirection()
+    //    {
+    //        // Генерация случайных чисел от -1 до 1
+    //        float x = Random.Range(-1f, 1f);
+    //        float y = Random.Range(-1f, 1f);
+
+    //        // Нормализация вектора (чтобы длина была равна 1)
+    //        direction = new Vector2(x, y).normalized;
+    //    }*/
+    public float moveRadius = 10f; // Радиус, в пределах которого враг будет искать точки
+    public float checkDistance = 1f; // Минимальное расстояние от цели для смены точки
+    public LayerMask obstacleLayer; // Слой препятствий для проверки столкновений
+    public float movementSpeed = 3f; // Скорость перемещения врага
+
+    private Vector3 targetPoint;
+    new private Rigidbody2D rigidbody;
+
     void Start()
     {
+        FindNewTargetPoint();
         rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        MoveTowardsTarget();
+    }
+
+    private void FindNewTargetPoint()
+    {
+        bool pointFound = false;
+
+        while (!pointFound)
+        {
+            // Случайная точка в пределах радиуса
+            Vector3 randomDirection = Random.insideUnitSphere * moveRadius;
+            randomDirection += transform.position;
+            randomDirection.y = transform.position.y; // Учитываем только горизонтальную плоскость
+
+            // Проверка на препятствия
+            if (!Physics.Raycast(transform.position, randomDirection - transform.position, moveRadius, obstacleLayer))
+            {
+                targetPoint = randomDirection;
+                pointFound = true;
+            }
+        }
+    }
+
+    private void MoveTowardsTarget()
+    {
+        // Перемещение врага к целевой точке
+        Vector3 direction = (targetPoint - transform.position).normalized;
+        transform.position += direction * movementSpeed * Time.deltaTime;
+
+        // Проверка на достижение цели
+        if (Vector3.Distance(transform.position, targetPoint) < checkDistance)
+        {
+            FindNewTargetPoint(); // Смена цели
+        }
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.GetComponent<metla>())
         {
             rigidbody.gravityScale = 1;
-            speed = 0;
+            movementSpeed = 0;
         }
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        float r1 = Random.Range(-1f, 3f);
-        float r2 = Random.Range(-1f, 3f);
-        RandomMovement.x = r1 * speed;
-        RandomMovement.y = r2 * speed;
-
-
     }
 }
